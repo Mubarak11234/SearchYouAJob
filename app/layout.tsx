662e65742ff2f9f3010f9d1b2fe9cf716b246b35
mubarak <mubarakhasnain027@gmail.com>
2026-09-09 10:28:35 +0500

    Add Vercel analytics  Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "Your Website",
@@ -30,6 +31,7 @@ export default function RootLayout({
       <body suppressHydrationWarning={true}>
         {children}
         <SpeedInsights />
+        <Analytics />
         </body>
     </html>
   );
